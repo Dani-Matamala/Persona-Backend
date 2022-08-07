307b5654044146ea18781f8480e1703cd19f702d
Matamala Daniel <daniel20mat@gmail.com>
2022-08-07 16:53:29 -0300

    Correccion de errores

--- Persons-Backend/index.js
+++ Persons-Backend/index.js
@@ -10,69 +10,21 @@ const Person = require('./models/Person')
 
 // Middleware
 
-<<<<<<< HEAD
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendick',
-    number: '39-23-6423122'
-  }
-]
-=======
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
->>>>>>> 25558ed7ed2f1c6b9e4760a4a840dbaa8af90e90
 
 // Routes
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
+})
+
 app.get('/api/persons', (req, res) => {
-<<<<<<< HEAD
-  res.json(persons)
-})
-
-app.get('/info', (req, res) => {
-  const date = new Date()
-  const info = `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${date}</p>`
-  res.send(info)
-})
-
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  person ? res.json(person) : res.status(404).end()
-})
-
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  console.log(persons)
-  res.status(204).end()
-})
-
-app.post('/api/persons', (req, res) => {
-  const body = req.body
-  if (!body.name || !body.number) {
-=======
-  Person.find({})
-    .then(persons => {
-      res.json(persons)
-    })
+  // Person.find({})
+  //   .then(persons => {
+  //     res.json(persons)
+  //   })
+  res.send('<h1>Hello Persons</h1>')
 })
 
 app.get('/api/persons/:id', (req, res) => {
@@ -97,33 +49,10 @@ app.delete('/api/persons/:id', (req, res, next) => {
 app.post('/api/persons', (req, res, next) => {
   const body = req.body
   if (!body.name || !body.telephone) {
->>>>>>> 25558ed7ed2f1c6b9e4760a4a840dbaa8af90e90
     return res.status(400).json({
       error: 'name or number missing'
     })
   }
-<<<<<<< HEAD
-
-  const exitsPerson = persons.find(person => person.name === body.name)
-  if (exitsPerson) {
-    return res.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-
-  const person = {
-    id: Math.floor(Math.random() * 100000),
-    name: body.name,
-    number: body.number
-  }
-  persons = persons.concat(person)
-  console.log(persons)
-  res.json(person)
-})
-
-app.listen(3000, () => {
-  console.log('Server running on port 3000')
-=======
   const newPerson = new Person({
     name: body.name,
     telephone: body.telephone
@@ -157,10 +86,14 @@ app.put('/api/persons/:id', (req, res, next) => {
 // Middlewares Errors
 app.use(handleErrors)
 app.use(notFound)
+// middleware de manejo de rutas inexistentes
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-const PORT = process.env.PORT
+app.use(unknownEndpoint)
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
->>>>>>> 25558ed7ed2f1c6b9e4760a4a840dbaa8af90e90
 })
